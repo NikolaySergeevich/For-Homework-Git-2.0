41c7d7378aaa3f0009c97086b861d5ba8e212215
Nikolay <nik.pavlovskiy.1997@mail.ru>
2022-11-13 00:35:26 +0300

    Папка семинар + готовое дз задание 8

---
+++ Семинар/Program.cs
@@ -0,0 +1,6 @@
+﻿//Написать программу. которая принимает число и выдаёт его квадрат
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} = {sqr}");
