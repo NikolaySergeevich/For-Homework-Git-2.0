421be44780575b10c4bdb9e17d7047f0cbca4a53
Nikolay <nik.pavlovskiy.1997@mail.ru>
2022-11-13 00:05:54 +0300

    делал дз задачу 8

---
+++ ДЗ_к_семинару_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число N");
+// int N = Console.ReadLine();
+// // int number = 1;
+// // while (number <= N)
+// // {
+// // if (number%2 = 0)
+// //     {
+// //         Console.WriteLine(number);
+// //     }
+// //     number++;
+// // }
+// Console.WriteLine("Введите число N");
