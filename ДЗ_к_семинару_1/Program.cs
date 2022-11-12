53203acc79599a81d3a70c43667d892dba28fe1d
Nikolay <nik.pavlovskiy.1997@mail.ru>
2022-11-13 00:36:15 +0300

    дз 8 задание готово

--- ДЗ_к_семинару_1/Program.cs
+++ ДЗ_к_семинару_1/Program.cs
@@ -1,12 +1,11 @@
 ﻿Console.WriteLine("Введите число N");
-// int N = Console.ReadLine();
-// // int number = 1;
-// // while (number <= N)
-// // {
-// // if (number%2 = 0)
-// //     {
-// //         Console.WriteLine(number);
-// //     }
-// //     number++;
-// // }
-// Console.WriteLine("Введите число N");
+double N = int.Parse (Console.ReadLine()!);
+double number = 1;
+while (number <= N)
+{
+if (number%2 == 0)
+    {
+        Console.Write($"{number}, ");
+    }
+    number++;
+}
