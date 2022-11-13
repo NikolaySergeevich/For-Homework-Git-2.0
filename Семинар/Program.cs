6f59da2943a85e5d00eca5fc88993fae092c04b9
Nikolay <nik.pavlovskiy.1997@mail.ru>
2022-11-13 23:49:26 +0300

    Семинар 1

--- Семинар/Program.cs
+++ Семинар/Program.cs
@@ -1,10 +1,52 @@
 ﻿//Написать программу. которая принимает число и выдаёт его квадрат
-Console.WriteLine("Введите число");
-int number = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите число");
+// int number = int.Parse(Console.ReadLine()!);
 
-// int sqr = number * number;
-// Console.WriteLine($"Квадрат числа {number} = {sqr}");
+// // int sqr = number * number;
+// // Console.WriteLine($"Квадрат числа {number} = {sqr}");
 
 
-int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
-Console.WriteLine($"Квадрат числа {number} = {sqr1}");
+// int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
+// Console.WriteLine($"Квадрат числа {number} = {sqr1}");
+
+
+
+// написать программу принимающую на вход число N и потом выводит все числа от -N до N.
+
+// Console.WriteLine("Введите число");
+// int N = int.Parse(Console.ReadLine()!);
+// int N2 = -N;
+//  while (N2 <= N)
+//  {
+//     Console.Write($"{N2} ");
+//     N2++;
+//  }
+
+
+// Написать программу, кторая принимает трёхзначное число и выводит на экран последнюю цифру этого числа
+
+// Console.WriteLine("Введите трёхзначное число");
+// int N = int.Parse(Console.ReadLine()!);
+// int So = N%10;
+// Console.Write($"{So}");
+
+// Написать программу, которая принимает 2 числа и проверяет является ли первое число квадратом второго.
+
+// Console.WriteLine("Введите первое число число");
+// int number1 = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine("Введите второе число число");
+// int number2 = int.Parse(Console.ReadLine()!);
+
+// int sqr = Convert.ToInt32(Math.Pow(number2, 2));
+
+// // if (number1 == sqr)
+// // {
+// //     Console.WriteLine ("Да");
+// // }
+// // else 
+// // {
+// //     Console.WriteLine("Нет");
+// // }
+// bool lll = (number1 == sqr);
+// Console.WriteLine (lll);
