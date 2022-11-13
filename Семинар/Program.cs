b764a185514090a579379f9939606104f2ef965f
Nikolay <nik.pavlovskiy.1997@mail.ru>
2022-11-13 22:58:28 +0300

    Воспользовался обращением к библиотеке для воспользованием функии возведения в степень

--- Семинар/Program.cs
+++ Семинар/Program.cs
@@ -2,5 +2,9 @@
 Console.WriteLine("Введите число");
 int number = int.Parse(Console.ReadLine()!);
 
-int sqr = number * number;
-Console.WriteLine($"Квадрат числа {number} = {sqr}");
+// int sqr = number * number;
+// Console.WriteLine($"Квадрат числа {number} = {sqr}");
+
+
+int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа {number} = {sqr1}");
