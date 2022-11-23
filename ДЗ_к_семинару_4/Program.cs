cff49948adf01a82520e7caf13172c756b7c8ade
Nikolay <nik.pavlovskiy.1997@mail.ru>
2022-11-23 18:42:19 +0300

    ДЗ 4, 1 задача

---
+++ ДЗ_к_семинару_4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите первое чисол");
+double A = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе чисол");
+int B = int.Parse(Console.ReadLine()!);
+
+double Stepen(double A, int B)
+{
+    double result = A;
+    for (int i = 1; i < B; i++)
+    {
+        result = result * A;
+    }
+    return result;
+}
+
+double ytr = Stepen(A, B);
+Console.WriteLine($"Число {A} возведённое в степень {B} = {ytr}");
