4e6c04fe881d6cb848785249586efc0e9deda5d5
Nikolay <nik.pavlovskiy.1997@mail.ru>
2022-12-12 09:54:19 +0300

    23

--- ДЗ_к_семинару_8/Program.cs
+++ ДЗ_к_семинару_8/Program.cs
@@ -238,3 +238,45 @@ int[,] Matrix1Matrix2(int[,] Mat1, int[,] Mat2)
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
+
+int[,] CreatMas(int str, int col)
+{
+    int[,] Mas = new int[str,col];
+    for (int i = 0; i < Mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < Mas.GetLength(1); j++)
+        {
+            Mas[i,j] = 0;
+        }
+    }
+    return Mas;
+}
+void PrintMas(int[,] Ar)
+{
+    for (int i = 0; i < Ar.GetLength(0); i++)
+    {
+        for (int j = 0; j < Ar.GetLength(1); j++)
+        {
+            Console.Write($"{Ar[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Massiv = CreatMas(4,4);
+PrintMas(Massiv);
+Console.WriteLine();
+
+int[,] FinMas(int st, int co,int num)
+{
+    if (Massiv[st, co] == 0)
+    {
+    Massiv[st, co] = num;
+    FinMas(st, co, num + 1);
+    return Massiv;
+    }
+    return Massiv;
+}
+
+FinMas(0,0,1);
+PrintMas(Massiv);
